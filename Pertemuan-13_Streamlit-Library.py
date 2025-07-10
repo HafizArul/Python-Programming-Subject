a2c946c23b8d1f6740bd52993d0f07da08843bb2
HafizArul <hafizarul1604@outlook.co.id>
2025-07-10 08:40:20 +0700

    Mengubah file pertemuan-13_streamlit-library.py

--- Pertemuan-13_Streamlit-Library.py
+++ Pertemuan-13_Streamlit-Library.py
@@ -1,8 +1,28 @@
 import streamlit as st
-from streamlit_keplergl import keplergl
-from keplergl import Keplergl
+from st_cytoscape import cytoscape
 
-st.write("this is a kepler.gl map ini streamlit")
+elements = [
+    {"data": {"id": "X"}, "selected": True, "selectable": False},
+    {"data": {"id": "Y"}},
+    {"data": {"id": "Z"}},
+    {"data": {"source": "X", "target": "Y", "id": "X➞Y"}},
+    {"data": {"source": "Z", "target": "Y", "id": "Z➞Y"}},
+    {"data": {"source": "Z", "target": "X", "id": "Z➞X"}},
+]
 
-map_1 = KeplerGl
-keplergl_static(map_1)
+stylesheet = [
+    {"selector": "node", "style": {"label": "data(id)", "width": 20, "height": 20}},
+    {
+        "selector": "edge",
+        "style": {
+            "width": 3,
+            "curve-style": "bezier",
+            "target-arrow-shape": "triangle",
+        },
+    },
+]
+
+selected = cytoscape(elements, stylesheet, key="graph")
+
+st.markdown("**Selected nodes**: %s" % (", ".join(selected["nodes"])))
+st.markdown("**Selected edges**: %s" % (", ".join(selected["edges"])))
